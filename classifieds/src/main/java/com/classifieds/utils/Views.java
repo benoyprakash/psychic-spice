58404eacf206af7f605510b59b2f8009623b30d8
benoy <benoyprakash@gmail.com>
2014-06-08 23:35:05 +0530

    resolving conflicts

--- classifieds/src/main/java/com/classifieds/utils/Views.java
+++ classifieds/src/main/java/com/classifieds/utils/Views.java
@@ -4,4 +4,5 @@ public class Views {
 
 	public static final String HOME_VIEW = "home.view";
 	public static final String LOGIN_VIEW = "login.view";
+	public static final String LANDING_VIEW = "landing.view";
 }
