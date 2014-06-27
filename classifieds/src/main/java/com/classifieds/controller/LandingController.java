c84cc254450c8e861bc8d3a95f3f87c604f044e3
benoy <benoyprakash@gmail.com>
2014-06-27 22:24:11 +0530

    Landing Controller

---
+++ classifieds/src/main/java/com/classifieds/controller/LandingController.java
@@ -0,0 +1,23 @@
+package com.classifieds.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import com.classifieds.controller.BaseController;
+import static com.classifieds.utils.Views.LANDING_PAGE_VIEW;
+
+@Controller
+@RequestMapping(value = "/", method = { RequestMethod.GET, RequestMethod.POST })
+public class LandingController extends BaseController {
+
+	public ModelAndView landingPage(HttpServletRequest request) {
+		ModelMap model = new ModelMap();
+		model.put("header", "header");
+
+		return new ModelAndView(LANDING_PAGE_VIEW, model);
+	}
+}
