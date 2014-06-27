421022a242acc3376128a4e67ce54b98bdfea114
benoy <benoyprakash@gmail.com>
2014-06-27 22:24:21 +0530

    Landing Controller

--- classifieds/src/main/java/com/classifieds/utils/Views.java
+++ classifieds/src/main/java/com/classifieds/utils/Views.java
@@ -4,5 +4,5 @@ public class Views {
 
 	public static final String HOME_VIEW = "homepage.view";
 	public static final String LOGIN_VIEW = "login.view";
-	public static final String LANDING_VIEW = "landing.view";
+	public static final String LANDING_PAGE_VIEW = "landing.view";
 }
